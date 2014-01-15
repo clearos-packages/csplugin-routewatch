6e340be1df66ce8e615929dbffcf8623a2908b6f
Darryl Sokoloski <darryl@sokoloski.ca>
2014-01-15 10:55:11 -0500

    Fixed (simplified) autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,7 @@
 
 find $(pwd) -name configure.ac | xargs touch
 
-mkdir -vp m4
-
 # Regenerate configuration files
-aclocal -I m4 --install || exit 1
-autoheader || exit 1
-automake --foreign --include-deps --add-missing --copy || exit 1
-autoreconf -i -f || exit 1
+mkdir -vp m4
+autoreconf -i --force -I m4 || exit 1
 
-# Run configure for this platform
-#./configure $*
